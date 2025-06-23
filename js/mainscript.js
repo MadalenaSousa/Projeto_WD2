c8107a8af88d41830bf27ed3e307342146b7161a
Madalena Sousa <madalenaantunessousa@gmail.com>
2025-06-23 16:40:09 +0100

    Corrections

--- js/mainscript.js
+++ js/mainscript.js
@@ -23,6 +23,31 @@ exploreItems.forEach((item, index) => {
 
 //-----
 
+//ABOUT
+
+var exploreImg = document.querySelector(".explore-img");
+var exploreItems = document.querySelectorAll(".explore-items a");
+var exploreImgPath = [];
+
+exploreImg.src = "res/about/exploreitems/1.png";
+
+fetch("images.json")
+    .then(response => response.json())
+    .then(data => {
+        const about = data[5];
+        exploreImgPath = about[about.length - 1];
+    })
+    .catch(error => console.error("Error", error));
+
+
+exploreItems.forEach((item, index) => {
+    item.addEventListener("mouseenter", () => {
+        exploreImg.src = exploreImgPath[index].path;
+    });
+});
+
+//-----
+
 //CONTACTS
 
 var inputArray = querySelectorAll(".contact-form input");
